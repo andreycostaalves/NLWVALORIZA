f93f9cf132e758f2d61a09c9e791eb71283428b3
Andrey Alves <andreyalvesgit@gmail.com>
2021-06-22 17:14:47 -0300

    Iniciando API,conectando banco de dados SQLite com TypeORM

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import express, { request, response } from  "express";
+import "reflect-metadata";
+import  "./database";
+
+const app =  express();
+
+
+
+
+
+
+app.listen(3000, () => console.log("Server is running nlw"));
